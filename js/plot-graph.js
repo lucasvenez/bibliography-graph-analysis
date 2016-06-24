4890d4582442be65333b85d797e7a119d312f2fe
Lucas Venezian Povoa <lucasvenez@gmail.com>
2016-06-24 18:02:12 -0300

    Update layout

--- js/plot-graph.js
+++ js/plot-graph.js
@@ -9,9 +9,18 @@ var createGraph = require('ngraph.graph');
 
 var graph = createGraph({uniqueLinkIds: false});
 
-for (var i = 0; i < v.length; i++)
+var lastNode = "";
+
+for (var i = 0; i < v.length; i++) {
+
+	if (v[i].src != lastNode) {
+		graph.addNode(v[i].src);
+		lastNode = v[i].src;
+	}
+	
 	if (!graph.hasLink(v[i].src, v[i].dst))
         graph.addLink(v[i].src, v[i].dst);
+}
 
 var createLayout = require('ngraph.offline.layout');
 
